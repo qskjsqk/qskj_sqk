d406c0f6fcc8a3fed4c1675f98b974bdd8b64ccc
zhanggeng <zhanggeng@hongwangweb.com>
2018-07-31 14:10:40 +0800

    遗漏提交，ImagePathUtil图片地址判空处理

--- lysqk/app/src/main/java/com/zhcd/lysqk/tool/ImagePathUtil.java
+++ lysqk/app/src/main/java/com/zhcd/lysqk/tool/ImagePathUtil.java
@@ -1,10 +1,12 @@
 package com.zhcd.lysqk.tool;
 
+import android.text.TextUtils;
+
 import com.zhcd.lysqk.net.ServiceProvider;
 
 public class ImagePathUtil {
     public static String imageReallyUrl(String imageUrl) {
-        if (imageUrl.toLowerCase().contains("http")) {
+        if (!TextUtils.isEmpty(imageUrl) && imageUrl.toLowerCase().contains("http")) {
             return imageUrl;
         }
         return ServiceProvider.getImageBaseUrl() + imageUrl;
